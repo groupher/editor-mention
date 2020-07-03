c9e92d8ae5508ccd0592dbedce1cd64977cf6148
mydearxym <xieym@chinawyny.com>
2020-07-03 15:28:19 +0800

    refactor: some edge case when cancle mention popover

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import {
   keepCustomInlineToolOnly,
   restoreDefaultInlineTools,
   removeElementByClass,
+  insertHtmlAtCaret,
+  convertElementToTextIfNeed,
 } from '@groupher/editor-utils'
 import './index.css'
 
@@ -69,17 +71,17 @@ export default class Mention {
     })
 
     this.mentionInput.addEventListener('focus', () => {
-      const mention = document.querySelector('#' + this.CSS.mention)
-      console.log('focused', mention)
+      const mentionEl = document.querySelector('#' + this.CSS.mention)
 
-      if (mention) {
+      if (mentionEl) {
         const mentionCursorHolder = make('span', CSS.focusHolder)
-        mention.parentNode.insertBefore(
+        mentionEl.parentNode.insertBefore(
           mentionCursorHolder,
-          mention.nextSibling,
+          mentionEl.nextSibling,
         )
       }
     })
+
     /**
      * should clear anchors after user manually click outside the popover,
      * otherwise will confuse the next insert
@@ -91,13 +93,18 @@ export default class Mention {
      */
     this.mentionInput.addEventListener('blur', () => {
       this.selectionStateChecked = false
+
       setTimeout(() => {
         console.log("this.mentionInput on blur: ", this.mentionInput.value)
+        const mentionEl = document.querySelector('#' + this.CSS.mention)
+        console.log('blurred: ', mentionEl)
+
         if (this.mentionInput.value.trim() === '') {
-          this.closePopover()
+          this.cleanUp()
         }
       }, 300)
-    })
+    }) // blur end
+
     this.selectionStateChecked = false
 
     this.mentionContainer.appendChild(this.mentionInput)
@@ -115,8 +122,16 @@ export default class Mention {
    * @return {void}
    */
   handleMentionInput(ev) {
-    if (ev.code === 'Escape') return this.closePopover()
-    if (ev.code === 'Enter') return console.log('select first item')
+    if (ev.code === 'Escape') {
+      // clear the mention input and close the toolbar
+      this.mentionInput.value = ''
+      this.cleanUp()
+      return
+    }
+
+    if (ev.code === 'Enter') {
+      return console.log('select first item')
+    }
 
     const user = {
       id: 1,
@@ -136,7 +151,7 @@ export default class Mention {
    * @return {HTMLElement}
    */
   makeSuggestion(user) {
-    const mention = document.querySelector('#' + this.CSS.mention)
+    const mentionEl = document.querySelector('#' + this.CSS.mention)
     const suggestionWrapper = make('div', [this.CSS.suggestion], {})
 
     const avatar = make('img', [this.CSS.mentionAvatar], {
@@ -158,8 +173,8 @@ export default class Mention {
 
     suggestionWrapper.addEventListener('click', () => {
       this.mentionInput.value = user.title
-      mention.innerHTML = user.title
-      this.closePopover()
+      mentionEl.innerHTML = user.title
+      this.cleanUp()
     })
 
     // https://avatars0.githubusercontent.com/u/6184465?s=40&v=4
@@ -172,24 +187,24 @@ export default class Mention {
    *
    * @return {void}
    */
-  closePopover() {
-    const mention = document.querySelector('#' + this.CSS.mention)
-    if (!mention) return
+  cleanUp() {
+    const mentionEl = document.querySelector('#' + this.CSS.mention)
+    if (!mentionEl) return
 
     // empty the mention input
     // this.mentionInput.value = ''
     this.clearSuggestions()
 
+    // closePopover
     const inlineToolBar = document.querySelector('.' + this.CSS.inlineToolBar)
-
     // this.api.toolbar.close is not work
     // so close the toolbar by remove the open class manually
     // this.api.toolbar.close()
     inlineToolBar.classList.remove(this.CSS.inlineToolBarOpen)
 
-    // moveCaretToEnd(mention.nextElementSibling)
-    if (mention.nextElementSibling) {
-      moveCaretToEnd(mention.nextElementSibling)
+    // move caret to end of the current mention
+    if (mentionEl.nextElementSibling) {
+      moveCaretToEnd(mentionEl.nextElementSibling)
     }
 
     // mention holder id should be uniq
@@ -197,7 +212,7 @@ export default class Mention {
     setTimeout(() => {
       this.removeAllHolderIds()
       removeElementByClass(CSS.focusHolder)
-      // document.querySelector(`.${CSS.focusHolder}`).remove()
+      convertElementToTextIfNeed(mentionEl, this.mentionInput)
     }, 50)
   }
 
@@ -207,9 +222,9 @@ export default class Mention {
    * @return {HTMLElement}
    */
   render() {
-    const emptyDiv = make('div', [this.CSS.mentionToolbarBlock], {})
+    const emptyEl = make('div', [this.CSS.mentionToolbarBlock], {})
 
-    return emptyDiv
+    return emptyEl
   }
 
   /**
